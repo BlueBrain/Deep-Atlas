2dada5979d27a22aa9790e0c54b1fed23843b589
EmilieDel <47669575+EmilieDel@users.noreply.github.com>
2022-07-28 13:48:32 +0200

    Put every parameters optional (#63)

--- pipeline/full_pipeline.py
+++ pipeline/full_pipeline.py
@@ -35,35 +35,39 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        "nissl_path",
+        "--nissl-path",
         type=Path,
+        required=True,
         help="""\
         Path to Nissl Volume.
         """,
     )
     parser.add_argument(
-        "ccfv2_path",
+        "--ccfv2-path",
         type=Path,
+        required=True,
         help="""\
         Path to CCFv2 Volume.
         """,
     )
     parser.add_argument(
-        "experiment_id",
+        "--experiment-id",
         type=int,
+        required=True,
         help="""\
         Experiment ID from Allen Brain to use.
         """,
     )
     parser.add_argument(
-        "output_dir",
+        "--output-dir",
         type=Path,
+        required=True,
         help="""\
         Path to directory where to save the results.
         """,
     )
     parser.add_argument(
-        "--ccfv3_path",
+        "--ccfv3-path",
         type=Path,
         help="""\
         Path to CCFv3 Volume.
