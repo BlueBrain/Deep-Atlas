55dd86032342eefb64638472c3b9e6fa0458f3ad
EmilieDel <47669575+EmilieDel@users.noreply.github.com>
2023-02-02 15:38:07 +0100

    Handle ITK Error (#73) (#74)

--- pipeline/gene_to_nissl.py
+++ pipeline/gene_to_nissl.py
@@ -107,11 +107,13 @@ def registration(
     section_numbers_kept = []
     for i, (section_number, gene_slice) in enumerate(zip(section_numbers, gene_volume)):
         try:
+            if section_number < 0:
+                raise IndexError("The index is negative.")
             nissl_slice = nissl_volume[int(section_number)]
         except IndexError:
             logger.warn(
-                f"One of the gene slice has a section number ({section_number})"
-                f"out of nissl volume shape {nissl_volume.shape}. This slice is"
+                f"One of the gene slice has a section number ({section_number}) "
+                f"out of nissl volume shape {nissl_volume.shape}. This slice is "
                 "removed from the pipeline."
             )
             section_numbers_kept.append(False)
